43e55a62a2ce1728eeb0140aad5ccd3b6da8f8e4
Luis Reyes <luisrogelio98@gmail.com>
2019-08-21 16:34:29 -0400

    modelo de materia prima

--- estimator/raw_materials/models.py
+++ estimator/raw_materials/models.py
@@ -3,16 +3,51 @@ from django.utils.translation import ugettext_lazy as translate
 # Modelos necesarios para la materia prima
 
 
+class Provider(models.Model):
+    """ Modelo de los proveedores """
+    # Campos propios
+    # Nombre
+    name = models.CharField(
+        translate("Name"),
+        max_length=50
+    )
+
+    # Referencias
+    company = models.ForeignKey(
+        "users.Company",
+        verbose_name=translate("Company"),
+        on_delete=models.CASCADE
+    )
+
+    raw_materials = models.ManyToManyField(
+        "RawMaterial",
+        verbose_name=translate("Raw Materials")
+    )
+
+    class Meta:
+        verbose_name = translate("Provider")
+        verbose_name_plural = translate("Providers")
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("Provider_detail", kwargs={"pk": self.pk})
+
+
 class RawMaterial(models.Model):
     """Modelo de Materia prima"""
+    TON = 'T'
     KILOGRAM = 'Kg'
     GRAM = 'g'
     MILLIGRAM = 'mg'
     LITER = 'l'
     MILLILITER = 'ml'
     UNIT = 'u'
+
     # measurement unit
     MEASUREMENT_UNITS = [
+        (TON, translate('Ton')),
         (KILOGRAM, translate('Kilograms')),
         (GRAM, translate('grams')),
         (MILLIGRAM, translate('milligrams')),
@@ -42,19 +77,40 @@ class RawMaterial(models.Model):
         blank=False,
         default=False,
     )
+    # Tiempo en vencerse almacenado como un datetime.timedelta de python
+    time_to_expire = models.DurationField(
+        translate("Time to expire"),
+        blank=True,
+    )
+
     # es importada?
     is_imported = models.BooleanField(
         translate('is imported'),
         blank=False,
         default=False,
     )
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    # Campos Referencias
+    # Muchos proveedores
+    providers = models.ManyToManyField(
+        Provider,
+        verbose_name=translate("Providers")
+    )
+    # una compañia
+    company = models.ForeignKey(
+        "users.Company",
+        verbose_name=translate("Company"),
+        on_delete=models.CASCADE
+    )
 
     class Meta:
         verbose_name = translate("Raw Material")
         verbose_name_plural = translate("Raw Materials")
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
 
     # def get_absolute_url(self):
     #     return reverse("RawMaterial_detail", kwargs={"pk": self.pk})
