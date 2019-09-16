32fca249545cd819bbe89ba7cd8f8452a3d603df
Luis Reyes <luisrogelio98@gmail.com>
2019-09-16 19:47:50 -0400

    barra de navegacion en estado constante

--- estimator/api/views.py
+++ estimator/api/views.py
@@ -1,22 +1,24 @@
 import json
 
 from django.views.generic import View
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
-class SessionVariablesView( View):
+class SessionVariablesView(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
-        # Code for GET requests
+        # Get el valor de la variable de sesion del nombre del url
         variable_name = self.kwargs['session_var']
-        return JsonResponse({'foo': variable_name})
+        response = ""
+        if variable_name in request.session:
+            response = request.session[variable_name]
+        return JsonResponse({'value': response})
 
     def post(self, request, *args, **kwargs):
-        # Code for POST requests
+        # set la variable de sesion del nombre del url
+        value = json.loads(request.body)['value']
         variable_name = self.kwargs['session_var']
-        print(variable_name)
-        received_json_data = json.loads(request.body)
-        print(received_json_data)
-        return JsonResponse({'post': variable_name})
+        request.session[variable_name] = value
+        return HttpResponse(200)
--- estimator/static/js/navigation.js
+++ estimator/static/js/navigation.js
@@ -1,36 +1,49 @@
-var toggled_navigation = true;
-// var item = document.getElementsByClassName("app__item--hidden");
-// var item2 = document.getElementsByClassName("nav-item");
-// var item3 = document.getElementsByClassName("app__nav--link");
-
 function mostrar() {
   let content = document.getElementById("content");
   let sidebar = document.getElementById("sidebar");
-  if (toggled_navigation) {
 
-    content.classList.toggle("active");
-    sidebar.classList.toggle("nav__toggled")
+  
+  if (sidebar_extended) {
+    content.classList.remove("active");
+    sidebar.classList.remove("nav__toggled");
 
-  } else {
-    sidebar.classList.toggle("nav__toggled")
-    content.classList.toggle("active");
-
+  }else{
+    content.classList.add("active");
+    sidebar.classList.add("nav__toggled");
   }
-  toggled_navigation = !toggled_navigation;
-  postSidebarSession();
+
+  postSidebarSession(sidebar_extended);
+  sidebar_extended = !sidebar_extended;
 }
 
-async function postSidebarSession() {
-  let response = await fetch('/apiv1/session/sidebar/', {
-    method: 'POST',
-    body: {
-      'hola': 'hola'
-    },
-    headers: {
-      'Content-Type': 'application/json'
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-  }).then(function (res) {
-    console.log(res);
   }
-  );
+  return cookieValue;
 }
+
+async function postSidebarSession(value) {
+  let response = await fetch('/apiv1/session/sidebar_extended/', {
+    method: 'POST',
+    credentials: "same-origin",
+    headers: {
+      "X-CSRFToken": getCookie("csrftoken"),
+      "Accept": "application/json",
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'value': value,
+    }),
+  });
+}
+mostrar();
