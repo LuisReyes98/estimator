4e902abbc8dad91734a9f261314c285e554f5950
Luis Reyes <luisrogelio98@gmail.com>
2019-10-01 00:03:18 -0400

    limpieza datos nombres

--- data_de_prueba/formater.py
+++ data_de_prueba/formater.py
@@ -3,8 +3,6 @@ import datetime
 import pandas
 import copy
 
-file_lines = []
-
 file_headers = [
     'fecha',
     'material',
@@ -17,25 +15,96 @@ file_headers = [
 
 FILE_PATH = 'datos_de_ventas.csv'
 
+
 def read_ventas():
+    file_lines = []
 
     csv.register_dialect('semi_col', delimiter=';', quoting=csv.QUOTE_NONE)
     reader = csv.reader(open(FILE_PATH, 'r'), 'semi_col')
-    for row in reader:
-        # row[1] = "hola"
-        # for val in row:
-        #     row_values.append(val)
-        file_lines.append(copy.copy(row))
-        print(row)
-    # data = pandas.read_csv(
-    #     'datos_de_ventas.csv',
-    #     index_col='fecha',
-    #     parse_dates=['fecha'],
-    #     header=0,
-    #     names=file_headers
-    # )
-    # print(data)
+    for index, row in enumerate(reader):
+        if index > 0:
+            pass
+            # agrupando franelillas
+            if row[1].find('FRANELILLA') != -1:
+                row[1] = 'FRANELILLA'
+
+            elif row[1].find('NIÑO') != -1:
+                row[1] = 'FRANELA NIÑO'
+
+            elif row[1].find('FRANELA') != -1:
+                row[1] = 'FRANELA'
+
+            elif row[1].find('TAZA') != -1:
+                row[1] = 'TAZA'
+
+            elif row[1].find('VASO') != -1:
+                row[1] = 'VASO'
+
+            elif row[1].find('GORRA') != -1:
+                row[1] = 'GORRA'
+
+            elif row[1].find('PULSERA') != -1:
+                row[1] = 'PULSERA'
+
+            elif row[1].find('PULSERITA') != -1:
+                row[1] = 'PULSERITA'
+
+            elif row[1].find('BODY') != -1:
+                row[1] = 'BODY BEBE'
+
+            elif row[1].find('CAB') != -1:
+                row[1] = 'FRANELA'
+
+            elif row[1].find('DAMAS') != -1:
+                row[1] = 'FRANELA'
+
+            elif row[1].find('CARTUCHERA') != -1:
+                row[1] = 'CARTUCHERA'
+
+            elif row[1].find('LLAVERO') != -1:
+                row[1] = 'LLAVERO'
+
+            elif row[1].find('VIAJERA') != -1:
+                row[1] = 'VIAJERA'
+
+            elif row[1].find('PORTA') != -1:
+                row[1] = 'PORTA COSMETICO'
+
+            elif row[1].find('BRAZALETE') != -1:
+                row[1] = 'BRAZALETE'
+
+            elif row[1].find('ESTAMPADO') != -1:
+                row[1] = 'ESTAMPADO'
+
+            elif row[1].find('BANDOLERA') != -1:
+                row[1] = 'BANDOLERA'
+
+            elif row[1].find('TULA') != -1:
+                row[1] = 'TULA'
+
+            elif row[1].find('MAPA') != -1:
+                row[1] = 'COLLAR MAPA'
+            file_lines.append(copy.copy(row))
+
+    return file_lines
+
+
+def write_csv(rows, file_name):
+
+    with open(file_name, mode='w') as employee_file:
+        file_writer = csv.writer(
+            employee_file,
+            delimiter=';',
+            quotechar='"',
+            quoting=csv.QUOTE_MINIMAL
+        )
+
+        file_writer.writerow(file_headers)
+
+        for row in rows:
+            file_writer.writerow(row)
 
 
 if __name__ == "__main__":
-    read_ventas()
+    rows = read_ventas()
+    write_csv(rows, 'datos_formateados.csv')
